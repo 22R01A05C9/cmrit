8260231b898b1bc54902ca650fca46a4f6ae19ac
saitej aageera <ageerasaiteja@gmail.com>
2023-11-05 17:49:32 +0530

    first

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template,request
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/display', methods=['POST'])
+def display():
+    rollno=request.form['roll']
+    roll=rollno.upper()
+    url='https://dhondi.cmrithyderabad.edu.in/sharedfiles/e0d341de643812c29a19aac35b9e7d87/studentPhotos/'+roll+'.jpg'
+    return render_template('home.html',photo=True,li=url)
+if __name__=='__main__':
+    app.run(debug=True,host='192.168.0.105')
